019211a87476932e6186c6bdc66fee9df9882afd
mnlrc <manuelrocca05@gmail.com>
2025-10-07 17:59:46 +0200

    Six more tests passed

--- src/problem/corner_problem.py
+++ src/problem/corner_problem.py
@@ -9,7 +9,7 @@ class CornerState(WorldState):
         gems_collected: list[bool],
         agents_alive: list[bool],
         corner_positions: set[tuple[int, int]],
-        visited_corners: set[tuple[int, int]],
+        visited_corners: set[tuple[int, int]] = set(),
     ):
         super().__init__(agents_positions, gems_collected, agents_alive)
         self.corner_positions = corner_positions
@@ -28,7 +28,7 @@ class CornerState(WorldState):
         return instance
 
     def __hash__(self) -> int:
-        return hash(self.corner_positions, self.visited_corners)
+            return hash(super().__hash__() and tuple(self.visited_corners))
     
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, CornerState):
@@ -39,17 +39,29 @@ class CornerProblem(SearchProblem[CornerState]):
     def __init__(self, world: World):
         super().__init__(world)
         self.corners = [(0, 0), (0, world.width - 1), (world.height - 1, 0), (world.height - 1, world.width - 1)]
-        # self.initial_state = CornerState(...)
+        initial_world_state = world.get_state()
+        self.initial_state = CornerState(
+            initial_world_state.agents_positions,
+            initial_world_state.gems_collected,
+            initial_world_state.agents_alive,
+            self.corners,
+        )
 
     def is_goal_state(self, state: CornerState) -> bool:
         # 2 steps:
-        # 1. -> check corners (if they all have been visited)
-        # 2. -> check exits, basically same code than before
+        # 1st -> check corners (if they all have been visited)
+        # 2nd -> check exits, basically same code than before
 
-        # 1. step
+        # 1st step
+        # if all the exits haven't been visited, there is no use in checking the exits
+
+        if not isinstance(state, CornerState):
+            return False 
+
+        if not state.corner_positions == state.visited_corners:
+            return False
         
-
-        # 2. step
+        # 2nd step
         if not any(state.agents_alive):
             return False
         tem_agents_pos = state.agents_positions.copy()
@@ -65,8 +77,23 @@ class CornerProblem(SearchProblem[CornerState]):
 
     def get_successors(self, state: CornerState):
         successors = []
+
+        if not isinstance(state, CornerState):
+            state = CornerState(
+                state.agents_positions,
+                state.gems_collected,
+                state.agents_alive,
+                self.corners,
+                set()
+            )
+
         for world_state, actions in super().get_successors(state):
-            # You probably need to do something like this here
-            # next_state = CornerState(...)
-            raise NotImplementedError()
+            next_state = CornerState(
+                world_state.agents_positions,
+                world_state.gems_collected,
+                world_state.agents_alive,
+                state.corner_positions,
+                state.visited_corners
+            )
+            successors.append((next_state, actions))
         return successors
